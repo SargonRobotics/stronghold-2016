4c53a93fb4e0b6a3a599c61ebfa0eb0cefc5786d
SargonRobotics <smerobotics@gmail.com>
2016-01-26 05:25:11 -0600

    first xbox code and attempt at deadzone.

--- src/Robot.cpp
+++ src/Robot.cpp
@@ -1,6 +1,10 @@
 #include "WPILib.h"
 
+#define ISJOYSTICK 0
+#define DEADZONETEST 1
+
 class Robot: public IterativeRobot {
+#if ISJOYSTICK
 	enum joystick {
 		MAINJOY = 0
 	};
@@ -8,6 +12,25 @@ class Robot: public IterativeRobot {
 	enum buttons {
 		SHOOTBALL = 1, ARMUP = 6, ARMDOWN = 4
 	};
+
+	enum axis {
+
+	};
+
+#else
+	enum joystick {
+		MAINJOY = 0
+	};
+
+	enum buttons {
+
+	};
+
+	enum axis {
+		SHOOTBALL = 3, ARMDIRECTION = 5, MOVE = 1, ROTATE = 0
+	};
+
+#endif
 
 	enum motors {
 		LEFTDRIVE = 0,
@@ -23,7 +46,7 @@ class Robot: public IterativeRobot {
 //	};
 
 	RobotDrive myRobot; // robot drive system
-	Joystick stick; // only joystick
+	Joystick controller; // only joystick
 	JoystickButton rollerButton;
 	Talon leftRollerMotor;
 	Talon rightRollerMotor;
@@ -31,8 +54,8 @@ public:
 
 	Robot() :
 			myRobot(LEFTDRIVE, RIGHTDRIVE),	// initialize the RobotDrive to use motor controllers on ports 0 and 1
-			stick(MAINJOY),
-			rollerButton(&stick, SHOOTBALL),
+			controller(MAINJOY),
+			rollerButton(&controller, SHOOTBALL),
 			leftRollerMotor(LEFTROLLERS),
 			rightRollerMotor(RIGHTROLLERS)
 	{
@@ -67,7 +90,6 @@ private:
 	void AutonomousInit() {
 		autoSelected = *((std::string*) chooser->GetSelected());
 		//std::string autoSelected = SmartDashboard::GetString("Auto Selector", autoNameDefault);
-		std::cout << "Auto selected: " << autoSelected << std::endl;
 
 		if (autoSelected == autoNameCustom) {
 			//Custom Auto goes here
@@ -85,22 +107,76 @@ private:
 	}
 
 	void TeleopInit() {
-
 	}
 
 	void TeleopPeriodic() {
+#if JOYSTICK
 		//Driving
-		myRobot.ArcadeDrive(stick);
+		myRobot.ArcadeDrive(controller);
 
 		//Control for ball capture/shooting.
-//		if (rollerButton.Get()){
-//			leftRollerMotor.Set(1);
-//			rightRollerMotor.Set(1);
-//		}
-//		else {
-//			leftRollerMotor.Set(-1);
-//			rightRollerMotor.Set(-1);
-//		}
+		//		if (rollerButton.Get()){
+		//			leftRollerMotor.Set(1);
+		//			rightRollerMotor.Set(1);
+		//		}
+		//		else {
+		//			leftRollerMotor.Set(-1);
+		//			rightRollerMotor.Set(-1);
+		//		}
+
+#else
+		//Setting up the axes
+		double rightTrigger = controller.GetRawAxis(SHOOTBALL);
+		double rightStickY = controller.GetRawAxis(ARMDIRECTION);
+		double moveDirection = controller.GetRawAxis(MOVE);
+		double rotateAmount = controller.GetRawAxis(ROTATE);
+
+#if DEADZONETEST
+		if(abs(moveDirection) < 0.2){
+			moveDirection = 0;
+		} else {
+			if(moveDirection < 0){
+				moveDirection += 0.2;
+			} else {
+				moveDirection -= 0.2;
+			}
+		}
+
+		if(abs(rotateAmount) < 0.2){
+				rotateAmount = 0;
+				} else {
+					if(rotateAmount < 0){
+						rotateAmount += 0.2;
+					} else {
+						rotateAmount -= 0.2;
+					}
+				}
+#endif
+
+		if(rightTrigger == 1){
+			//TODO: have shootBall method
+		}
+
+		if(rightStickY < 0.2){
+			//TODO: have moveArmUp method
+		} else if (rightStickY > -0.2){
+			//TODO: have moveArmDown method
+		}
+
+
+
+		//moveDirection = abs(moveDirection) < 0.8 ? 0 : (moveDirection - 0.8) / 0.2;
+		//rotateAmount = abs(rotateAmount) < 0.8 ? 0 : (rotateAmount - 0.8) / 0.2;
+
+		/*SmartDashboard testDashboard;
+
+		std::string pointToVal = (std::string) moveDirection;
+		std::string output = "DB/String Val: ";*/
+
+		std::string dashData = testDashboard.GetString(output, pointToVal);
+
+		myRobot.ArcadeDrive(moveDirection, rotateAmount, false);
+#endif
 
 		//Arm control.
 	}
