a8125b14991171477c9f9346381fe3cb7b8c70c3
SargonRobotics <smerobotics@gmail.com>
2016-02-09 20:09:05 -0600

    updated camera code and removed irrelevant bits from branch

--- src/Robot.cpp
+++ src/Robot.cpp
@@ -26,7 +26,7 @@ class Robot: public IterativeRobot {
 	};
 
 	enum buttons {
-
+		LIGHTS = 4
 	};
 
 	enum axis {
@@ -39,6 +39,7 @@ class Robot: public IterativeRobot {
 		LEFTDRIVE = 0, RIGHTDRIVE = 1,
 		LEFTARM = 2, RIGHTARM = 3,
 		LEFTROLLERS = 4, RIGHTROLLERS = 5,
+		LIGHTSWITCH = 8,
 	};
 
 	enum inputs {
@@ -49,168 +50,83 @@ class Robot: public IterativeRobot {
 //		MINARM = 0, LEFTARM = 1, RIGHTARM = 2
 //	};
 
-	AnalogInput rightArmPotInput;
-	AnalogInput leftArmPotInput;
-	RobotDrive myRobot; // robot drive system
+//IMAQdx Image Processing
+	IMAQdxSession session;
+	Image *frame;
+	IMAQdxError imaqError;
+	Relay lightSwitch;
 	Joystick controller; // only joystick
-	JoystickButton rollerButton;
-	Talon leftRollerMotor;
-	Talon rightRollerMotor;
-	Talon rightArmPotMotor;
-	Talon leftArmPotMotor;
-public:
+	JoystickButton lightsButton;
 
+public:
 	Robot() :
-			myRobot(LEFTDRIVE, RIGHTDRIVE),	// initialize the RobotDrive to use motor controllers on ports 0 and 1
-			controller(MAINJOY),
-			rollerButton(&controller, SHOOTBALL),
-			leftRollerMotor(LEFTROLLERS),
-			rightRollerMotor(RIGHTROLLERS),
-			rightArmPotInput(RIGHTPOTCHANNEL),
-			rightArmPotMotor(RIGHTARM),
-			leftArmPotInput(LEFTPOTCHANNEL),
-			leftArmPotMotor(LEFTARM)
+		lightSwitch(LIGHTSWITCH),
+		controller(MAINJOY),
+		lightsButton(&controller, LIGHTS)
 	{
-		myRobot.SetExpiration(0.1);
-		//myRobot.SetInvertedMotor()
-		//myRobot.SetInvertedMotor(LEFTDRIVE, true);
 	}
 
 private:
-	LiveWindow *lw = LiveWindow::GetInstance();
-	SendableChooser *chooser;
-	const std::string autoNameDefault = "Default";
-	const std::string autoNameCustom = "My Auto";
-	std::string autoSelected;
 
 	void RobotInit() {
-		chooser = new SendableChooser();
-		chooser->AddDefault(autoNameDefault, (void*) &autoNameDefault);
-		chooser->AddObject(autoNameCustom, (void*) &autoNameCustom);
-		SmartDashboard::PutData("Auto Modes", chooser);
-		CameraServer::GetInstance()->SetQuality(50);
-		std::shared_ptr<USBCamera> camera(new USBCamera ("cam0" , true));
-		camera->SetExposureManual(30);
-		CameraServer::GetInstance()->StartAutomaticCapture("cam0");
+//		CameraServer::GetInstance()->SetQuality(50);
+//		std::shared_ptr<USBCamera> camera(new USBCamera ("cam0" , true));
+//		camera->SetExposureManual(30);
+//		CameraServer::GetInstance()->StartAutomaticCapture("cam0");
+
+		 //IMAQdx Imaging
+		frame = imaqCreateImage(IMAQ_IMAGE_RGB, 0);
+		//the camera name (ex "cam0") can be found through the roborio web interface
+		imaqError = IMAQdxOpenCamera("cam0", IMAQdxCameraControlModeController, &session);
+		if(imaqError != IMAQdxErrorSuccess) {
+			DriverStation::ReportError("IMAQdxOpenCamera error: " + std::to_string((long)imaqError) + "\n");
+		}
+		imaqError = IMAQdxConfigureGrab(session);
+		if(imaqError != IMAQdxErrorSuccess) {
+			DriverStation::ReportError("IMAQdxConfigureGrab error: " + std::to_string((long)imaqError) + "\n");
+		}
 
 	}
 
-	/**
-	 * This autonomous (along with the chooser code above) shows how to select between different autonomous modes
-	 * using the dashboard. The sendable chooser code works with the Java SmartDashboard. If you prefer the LabVIEW
-	 * Dashboard, remove all of the chooser code and uncomment the GetString line to get the auto name from the text box
-	 * below the Gyro
-	 *
-	 * You can add additional auto modes by adding additional comparisons to the if-else structure below with additional strings.
-	 * If using the SendableChooser make sure to add them to the chooser code above as well.
-	 */
 	void AutonomousInit() {
-		autoSelected = *((std::string*) chooser->GetSelected());
-		//std::string autoSelected = SmartDashboard::GetString("Auto Selector", autoNameDefault);
 
-		if (autoSelected == autoNameCustom) {
-			//Custom Auto goes here
-		} else {
-			//Default Auto goes here
-		}
 	}
 
 	void AutonomousPeriodic() {
-		if (autoSelected == autoNameCustom) {
-			//Custom Auto goes here
-		} else {
-			//Default Auto goes here
-		}
+
 	}
 
 	void TeleopInit() {
 	}
 
 	void TeleopPeriodic() {
-#if JOYSTICK
-		//Driving
-		myRobot.ArcadeDrive(controller);
+		bool lightitup = controller.GetRawButton(4);
+		//IMAQdx Image Processing
+		IMAQdxStartAcquisition(session);
+		while(IsOperatorControl() && IsEnabled()) {
+			if (lightitup) {
+				lightSwitch.Set(Relay::kOn);
+			} else {
+				lightSwitch.Set(Relay::kOff);
+			}
 
-		//Control for ball capture/shooting.
-		//		if (rollerButton.Get()){
-		//			leftRollerMotor.Set(1);
-		//			rightRollerMotor.Set(1);
-		//		}
-		//		else {
-		//			leftRollerMotor.Set(-1);
-		//			rightRollerMotor.Set(-1);
-		//		}
-
-#else
-		//Setting up the axes
-		double rightTrigger = controller.GetRawAxis(SHOOTBALL);
-		double rightStickY = controller.GetRawAxis(ARMDIRECTION);
-		double moveDirection = controller.GetRawAxis(MOVE);
-		double rotateAmount = controller.GetRawAxis(ROTATE);
-		double armMovement = controller.GetRawAxis(ARMDIRECTION);
-
-#if DEBUG
-		std::string bDirection = std::to_string(moveDirection);
-		std::string bRotate = std::to_string(rotateAmount);
-		std::string bArm = std::to_string(armMovement);
-		SmartDashboard::PutString("DB/String 0", ("Dir before: " + bDirection));
-		SmartDashboard::PutString("DB/String 1", ("Rot before: " + bRotate));
-		SmartDashboard::PutString("DB/String 2", ("Arm before: " + bArm));
-#endif
-
-		moveDirection = createDeadzone(moveDirection);
-		rotateAmount = createDeadzone(rotateAmount);
-		armMovement = createDeadzone(armMovement);
-
-		double rCurrentPosition = rightArmPotInput.GetAverageVoltage(); //get position value
-//		motorSpeed = (currentPosition - currentSetpoint)*pGain; //convert position error to speed
-//		rightArmPotMotor.Set(motorSpeed); //drive elevator motor
-
-		double lCurrentPosition = rightArmPotInput.GetAverageVoltage(); //get position value
-		//		motorSpeed = (currentPosition - currentSetpoint)*pGain; //convert position error to speed
-		//		rightArmPotMotor.Set(motorSpeed); //drive elevator motor
-
-		//TODO: See debug todo below.
-		//TODO: Test the input we get from arm potentiometers to make sense of them.
-		//TODO: Very cautiously give the motor an input and hope it doesn't break.
-		//TODO: Add error checking for if the position of the arms differs by too much.
-		//TODO: Add controls for moving the arms together.
-
-#if DEBUG
-		std::string aDirection = std::to_string(moveDirection);
-		std::string aRotate = std::to_string(rotateAmount);
-		std::string aArm = std::to_string(armMovement);
-		std::string aRightPos = std::to_string(rCurrentPosition);
-		//TODO: Add output for other arm potentiometer
-		SmartDashboard::PutString("DB/String 3", ("Dir after: " + aDirection));
-		SmartDashboard::PutString("DB/String 4", ("Rot after: " + aRotate));
-		SmartDashboard::PutString("DB/String 5", ("Arm after: " + aArm));
-		SmartDashboard::PutString("DB/String 6", ("ArmPot: " + aRightPos));
-#endif
-
-		myRobot.ArcadeDrive(moveDirection, rotateAmount, false);
-#endif
-
-		//Arm control.
+			IMAQdxGrab(session, frame, true, NULL);
+			if(imaqError != IMAQdxErrorSuccess) {
+				DriverStation::ReportError("IMAQdxGrab error: " + std::to_string((long)imaqError) + "\n");
+			} else {
+				imaqDrawShapeOnImage(frame, frame, { 10, 10, 100, 100 }, DrawMode::IMAQ_DRAW_VALUE, ShapeMode::IMAQ_SHAPE_OVAL, 0.0f);
+				frcReadImage(frame,"/var/volatile/tmp/opencv-frame.png");
+				CameraServer::GetInstance()->SetImage(frame);
+			}
+			Wait(0.005);				// wait for a motor update time
+			}
+		     // stop image acquisition
+			IMAQdxStopAcquisition(session);
 	}
 
 	void TestPeriodic() {
-		lw->Run();
 	}
 
-	double createDeadzone(double amount, double deadzone=DEADZONE){
-		if(fabs(amount) < deadzone){
-			amount = 0;
-		} else {
-			if(amount < 0){
-				amount += deadzone;
-			} else {
-				amount -= deadzone;
-			}
-			amount /= (1 - deadzone);
-		}
-		return amount;
-	}
 };
 
 START_ROBOT_CLASS(Robot)
