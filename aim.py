03bc8da6a658262116351348ffe9e58ce93261be
Russell White <rarctem@gmail.com>
2016-02-13 15:49:44 -0500

    Add the expected angle to target to aiming script.

--- aim.py
+++ aim.py
@@ -3,10 +3,9 @@
 import math
 
 height = 2.464
-#height = 1
 init_v = 8.166
 
-print("Angle\tDist 1\tDist 2")
+print("Angle\tDist 1\tDist 2\tTarget1\tTarget2")
 for i in range(53, 81):
     angle = i / 180.0 * math.pi
     a = -9.8*.5
@@ -15,7 +14,9 @@ for i in range(53, 81):
     if b**2 - 4*a*c < 0:
         #print("No solution for angle {}\ta={:.4}\tb={:.4}\tc={:.4}\tsqrt={:.4}".format(i, a, b, c, b**2 - 4*a*c))
         continue
-    possibles = ((-b + math.sqrt(b**2 - 4*a*c))/(2*a),
-                 (-b - math.sqrt(b**2 - 4*a*c))/(2*a))
-    dists = list(map(lambda t: init_v*math.cos(angle)*t, possibles))
-    print("{}\t{:.2}\t{:.2}".format(i, dists[0], dists[1]))
+    times = ((-b + math.sqrt(b**2 - 4*a*c))/(2*a),
+             (-b - math.sqrt(b**2 - 4*a*c))/(2*a))
+    dists = list(map(lambda t: init_v * math.cos(angle) * t, times))
+    angles = list(map(lambda d: math.atan2(height, d) / math.pi * 180, dists))
+    print("{0}\t{1[0]:.2f}\t{1[1]:.2f}\t{2[0]:.2f}\t{2[1]:.2f}".format(i, dists, angles))
+
